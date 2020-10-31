7bd95acea13cba200ca0bb42a5a01e3140788019
123JackCole <123jack.cole@gmail.com>
2020-10-30 17:46:02 -0700

    added in quick sort in ruby

--- divide_and_conqueror.rb
+++ divide_and_conqueror.rb
@@ -1,18 +1,18 @@
 # Merge Sort
 def divide(arr)
-    return arr if arr.size < 2
+    return arr if arr.length < 2
 
-    center = arr.size / 2
+    center = arr.length / 2
     left = arr[0...center]
     right = arr[center..-1]
     
-    return sort(divide(left), divide(right))
+    sort(divide(left), divide(right))
 end
 
 def sort(left, right)
     result = [];
 
-    while (left.size > 0 && right.size > 0) do
+    while (left.length > 0 && right.length > 0) do
         result << (left[0] < right[0] ? left.shift() : right.shift())
     end
 
@@ -20,28 +20,22 @@ def sort(left, right)
 end
 
 # Quick Sort
-# const quickSort = (arr) => {
+def quickSort(arr)
 
-#     if (arr.length < 2) {
-#         return arr;
-#     }
+    return arr if arr.length < 2
 
-#     const index = arr.length - 1;
-#     const element = arr[index];
-#     const a = [];
-#     const b = [];
-#     let temp;
+    index = arr.length - 1
+    element = arr.pop
+    left, right = [], []
 
-#     for (let i = 0; i < index; i++) {
-#         temp = arr[i];
-#         temp < element ? a.push(temp) : b.push(temp);
-#     }
+    for current in arr
+        current < element ? left.push(current) : right.push(current)
+    end
 
-#     const output = [...quickSort(a), element, ...quickSort(b)]
-#     return output;
-# }
+    quickSort(left) + [element] + quickSort(right)
+end
 
 testArray = [9,5,2,6,8,3,4,1,7]
 
 puts divide(testArray)
-# puts merge_sort(testArray)
+puts quickSort(testArray)
